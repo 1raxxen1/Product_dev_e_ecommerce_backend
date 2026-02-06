b0822945c982455c638500fad2be28044a66ddf1
unknown <rutwij.soundalgekar001@gmail.com>
2026-02-06 23:54:02 +0530

    Added keywords and rich text ; Admin panel also changed

--- backend/apps/blogs/admin.py
+++ backend/apps/blogs/admin.py
@@ -1,13 +1,36 @@
 from django.contrib import admin
-from .models import BlogPost , BlogCategory
+from .models import BlogPost , BlogCategory , BlogContentBlock , Keyword
 
 # Register your models here.
-@admin.register(BlogPost)
-class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'is_published', 'created_at')
-    list_filter = ('is_published', 'category')
-    prepopulated_fields = {'slug': ('title',)}
 
+# Category Model
 @admin.register(BlogCategory)
 class BlogCategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name','slug')
+    search_fields = ('name',)
+
+# Keyword Model
+@admin.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name','slug')
+    search_fields = ('name',)
+
+# Content Block Model
+class BlogContentBlockInline(admin.TabularInline):
+    model = BlogContentBlock
+    extra = 1
+    fields = ("block_type", "rich_text", "image", "order")
+    ordering = ("order",)
+
+# Blog Post Model
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'is_published', 'created_at')
+    list_filter = ('is_published', 'category', 'keywords')
+    search_fields = ('title', 'excerpt')
+    filter_horizontal = ('keywords',)
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [BlogContentBlockInline]
+
--- backend/apps/blogs/models.py
+++ backend/apps/blogs/models.py
@@ -2,30 +2,68 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
+# to do
+# image-text-image 
+# keywords
 
 class BlogCategory(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True, blank=True)
 
-    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
+class Keyword(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nam
+
 
 class BlogPost(models.Model):
+
+    id = models.BigAutoField(primary_key=True)
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, blank=True)
     excerpt = models.TextField()
-    content = models.TextField()  # HTML or Markdown
-    cover_image = models.URLField(blank=True)
+
+    cover_image = models.ImageField(
+        upload_to='blog_covers/', 
+        null=True, blank=True
+    )
+
     category = models.ForeignKey(
         BlogCategory,
         on_delete=models.SET_NULL,
         null=True,
         related_name="posts"
     )
+
+    keywords = models.ManyToManyField(
+        Keyword,
+        related_name="posts",
+        blank=True
+    )
+
+
     is_published = models.BooleanField(default=True)
+    publised_at = models.DateTimeField(null=True, blank=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
@@ -35,3 +73,45 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+class BlogContentBlock(models.Model):
+    TEXT = "text"
+    IMAGE = "image"
+
+    BLOCK_TYPE_CHOICES = [
+        (TEXT, "Text"),
+        (IMAGE, "Image"),
+    ]
+
+    id = models.BigAutoField(primary_key=True)
+
+    post = models.ForeignKey(
+        BlogPost,
+        on_delete=models.CASCADE,
+        related_name="content_blocks"
+    )
+
+    block_type = models.CharField(
+        max_length=10,
+        choices=BLOCK_TYPE_CHOICES
+    )
+
+    rich_text = models.JSONField(blank=True, null=True)
+
+    image = models.ImageField(
+        upload_to="blog/blocks/",
+        blank=True,
+        null=True
+    )
+
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ["order"]
+        unique_together = ("post", "order")
+
+    def __str__(self):
+        return f"{self.post.title} - {self.block_type} [{self.order}]"
+    
+
+
