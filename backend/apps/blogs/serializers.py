5991fbe4dbb9546acb442e3abb89bf05811c6745
unknown <rutwij.soundalgekar001@gmail.com>
2026-02-07 23:22:11 +0530

    added serializers

--- backend/apps/blogs/serializers.py
+++ backend/apps/blogs/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
-from .models import BlogPost , BlogCategory
+from .models import BlogPost , BlogCategory , BlogContentBlock , Keyword
 
 class BlogCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogCategory
         fields = ['id', 'name', 'slug']
+
+class BlogkeywordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Keyword
+        fields = ['id', 'name', 'slug']
+
+class BlogContentBlockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogContentBlock
+        fields = ['id', 'block_type', 'rich_text', 'image', 'order']
 
 class BlogPostListSerializer(serializers.ModelSerializer):
     category = BlogCategorySerializer(read_only=True)
@@ -13,11 +23,30 @@ class BlogPostListSerializer(serializers.ModelSerializer):
         model = BlogPost
         fields = [
             'id', 'title', 'slug', 'excerpt',
-            'cover_image', 'category', 'created_at'
+            'cover_image', 'category', 'keywords', 'created_at'
         ]
+
 class BlogPostDetailSerializer(serializers.ModelSerializer):
     category = BlogCategorySerializer()
+    keywords = BlogkeywordSerializer(many=True, read_only=True)
+    content_blocks = BlogContentBlockSerializer(many=True, read_only=True)
 
     class Meta:
         model = BlogPost
-        fields = "__all__"
+        fields = [
+            'id', 'title', 'slug', 'excerpt', 'cover_image',
+            'category', 'keywords', 'content_blocks', 'created_at'
+        ]
+class BlogPostAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields =[
+            "id",
+            "title",
+            "slug",
+            "excerpt",
+            "cover_image",
+            "category",
+            "keywords",
+            "is_published",
+        ]
--- backend/apps/blogs/views.py
+++ backend/apps/blogs/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import BlogPost, BlogCategory
+from .models import BlogPost, BlogCategory , BlogContentBlock , Keyword
 from .serializers import (
     BlogPostListSerializer,
     BlogPostDetailSerializer,
     BlogCategorySerializer,
+    BlogContentBlockSerializer,
+    BlogkeywordSerializer,
+    BlogPostAdminSerializer,
 )
 
 # Create your views here.
+
 class BlogPostListView(generics.ListAPIView):
     queryset = BlogPost.objects.filter(is_published=True).order_by("-created_at")
     serializer_class = BlogPostListSerializer
