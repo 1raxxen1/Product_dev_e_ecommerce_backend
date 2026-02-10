c2be4d8fc0140207275003ecfd01b1ba110bd3b1
unknown <rutwij.soundalgekar001@gmail.com>
2026-02-10 20:45:03 +0530

    Serializers Done admin + public

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -37,7 +37,50 @@ class Variant(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sku = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.product.name} - {self.name}"
+    
+class Inventory(models.Model):
+    variant = models.OneToOneField(Variant, on_delete=models.CASCADE, related_name='inventory')
+    stock = models.PositiveIntegerField(default=0)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.variant.sku} | Stock: {self.stock}"
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image_url = models.ImageField(_("Image"), upload_to='product_images/', height_field=None, width_field=None, max_length=None)
+    alt_text = models.CharField(max_length=255, blank=True)
+
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return f"{self.product.name} Image {self.order}"
+    
+#Amazon types of products
+
+class ProductContentBlock(models.Model):
+    BLOCK_TYPES = [
+        ('text', 'Text'),
+        ('image', 'Image'),
+        ('spec', 'specification'),
+
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='content_blocks')
+
+    block_type = models.CharField(max_length=20, choices=BLOCK_TYPES)
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(_("image"), upload_to='product_content_blocks/', height_field=None, width_field=None, max_length=None)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return f"{self.product.name} Content Block {self.order}"
     
---
+++ backend/apps/products/serializers.py
@@ -0,0 +1,77 @@
+from rest_framework import serializers
+from .models import (ProductCategory, Product, Variant, Inventory, ProductImage , ProductContentBlock)
+
+
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = ['id', 'name', 'slug', 'parent']
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image_url', 'alt_text', 'order']
+
+class ProductContentBlockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductContentBlock
+        fields = ['id', 'product', 'block_type', 'content', 'order']
+
+class ProductVariantSerializer(serializers.ModelSerializer):
+    stock = serializers.IntegerField(source='inventory.stock', read_only=True)
+    class Meta:
+        model = Variant
+        fields = ['id', 'name', 'price', 'sku', 'stock']
+
+class ProductListSerializer(serializers.ModelSerializer):
+    category = ProductCategorySerializer(read_only=True)
+    images = ProductImageSerializer(many=True, read_only=True)
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'category', 'created_at', 'updated_at', 'is_active']
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = ProductCategorySerializer(read_only=True)
+    images = ProductImageSerializer(many=True, read_only=True)
+    content_blocks = ProductContentBlockSerializer(many=True, read_only=True)
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'category', 'created_at', 'updated_at', 'is_active']
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    category = ProductCategorySerializer(read_only=True)
+    images = ProductImageSerializer(many=True, read_only=True)
+    content_blocks = ProductContentBlockSerializer(many=True, read_only=True)
+    variants = ProductVariantSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'category', 'created_at', 'updated_at', 'is_active', 'images', 'content_blocks', 'variants']
+
+class ProductAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'category', 'created_at', 'updated_at', 'is_active' , 'slug']
+
+class ProductImageAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'product', 'image', 'alt_text', 'order']
+
+class ProductContentBlockAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductContentBlock
+        fields = ['id', 'product', 'block_type', 'content', 'image', 'order']
+
+class ProductVariantAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Variant
+        fields = ['id', 'product', 'name', 'price', 'sku', 'is_active']
+
+class InventoryAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = ['id', 'variant', 'stock', 'updated_at']
+
